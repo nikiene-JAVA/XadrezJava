607723fc52c2cffaaaddbc10956d22a688d55c7e
Nikobafos <nikolasnovo03@gmail.com>
2021-09-20 15:54:03 -0300

    Mostrando os movimentos possíveis

--- src/application/UI.java
+++ src/application/UI.java
@@ -59,6 +59,17 @@ public class UI {
 		}
 		System.out.println("  a b c d e f g h");
 	}
+	
+	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
+		for (int i = 0; i < pieces.length; i++) {
+			System.out.print((8 - i) + " ");
+			for (int j = 0; j < pieces.length; j++) {
+				printPiece(pieces[i][j], false);
+			}
+			System.out.println();
+		}
+		System.out.println("  a b c d e f g h");
+	}
 
 	private static void printPiece(ChessPiece piece, boolean background) {
 		if (background) {
