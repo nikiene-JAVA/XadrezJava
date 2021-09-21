f9a044446cbfebcc86f48e4c7d0a8df7f3437321
Nikobafos <nikolasnovo03@gmail.com>
2021-09-21 08:42:00 -0300

    Adicionando as peças

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -7,7 +7,11 @@ import java.util.stream.Collectors;
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
+import chess.pieces.Bishop;
 import chess.pieces.King;
+import chess.pieces.Knight;
+import chess.pieces.Pawn;
+import chess.pieces.Queen;
 import chess.pieces.Rook;
 
 public class ChessMatch {
